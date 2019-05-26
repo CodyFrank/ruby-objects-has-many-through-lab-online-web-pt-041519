764e50df1243c218acd575a3c251ebe226be7dd9
Cody Frank <cody.frank30@gmail.com>
2019-05-26 15:08:10 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,17 @@
+class Appointment
+
+  attr_reader :date, :patient, :doctor
+
+  def initialize(date, patient, doctor)
+    @@all << self
+    @date = date
+    @patient = patient
+    @doctor = doctor
+  end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+end
