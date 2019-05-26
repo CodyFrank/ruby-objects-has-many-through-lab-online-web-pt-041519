f53afa73b2c7c40e0605c82c4b6b8b10afe9cf66
Cody Frank <cody.frank30@gmail.com>
2019-05-26 15:21:32 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -10,4 +10,12 @@ class Patient
   def self.all
     @@all
   end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all
+  end
 end
